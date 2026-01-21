ac3712b218b06030f61e735f945809f40c828467
mohamedmaher <mohamedmaher.mm330@gmail.com>
2026-01-21 23:14:32 +0200

    fix: prettier and import sort conflict

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,27 +3,24 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import prettier from "eslint-config-prettier/flat";
 import ImportSorting from "eslint-plugin-simple-import-sort";
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   prettier,
   {
-    plugins: {
-      "simple-import-sort": ImportSorting,
-    },
-  },
-  // Override default ignores of eslint-config-next.
-  {
+    plugins: { "simple-import-sort": ImportSorting },
     rules: {
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "error",
       "simple-import-sort/imports": "error",
       "simple-import-sort/exports": "error",
-      // Optional: Next.js recommends disabling other sorting rules to avoid conflicts
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error",
       "sort-imports": "off",
       "import/order": "off",
     },
   },
+  // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
