26ee25f21b04f68a7e5e89ac262f587472fd4364
mohamedmaher <mohamedmaher.mm330@gmail.com>
2026-01-22 10:49:47 +0200

    refactor: use motion for support ssr

---
+++ components/WithClientAnimatedPresence.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { AnimatePresence } from "motion/react";
+import { FC, PropsWithChildren } from "react";
+const WithClientAnimatedPresence: FC<PropsWithChildren> = ({ children }) => {
+  return <AnimatePresence mode="popLayout">{children}</AnimatePresence>;
+};
+
+export default WithClientAnimatedPresence;
