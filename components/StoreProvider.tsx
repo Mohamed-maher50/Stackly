97051bca3e33b3edf8978f2f1336139b3198ce5a
mohamedmaher <mohamedmaher.mm330@gmail.com>
2026-01-26 00:10:16 +0200

    feat: integrate redux-persist and refactor store setup

--- components/StoreProvider.tsx
+++ components/StoreProvider.tsx
@@ -1,30 +1,29 @@
 "use client";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import { useEffect, useRef } from "react";
+import { LoaderIcon } from "lucide-react";
+import { useEffect } from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { AppStore, makeStore } from "@/lib/App.store";
+import { persistor, store } from "@/lib/App.store";
 
 export default function StoreProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>(undefined);
-  // eslint-disable-next-line react-hooks/refs
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore();
-  }
   useEffect(() => {
-    if (storeRef.current != null) {
-      // configure listeners using the provided defaults
-      // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
-      const unsubscribe = setupListeners(storeRef.current.dispatch);
-      return unsubscribe;
-    }
+    // configure listeners using the provided defaults
+    // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
+    const unsubscribe = setupListeners(store.dispatch);
+    return unsubscribe;
   }, []);
 
-  // eslint-disable-next-line react-hooks/refs
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={<LoaderIcon />}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- lib/App.store.ts
+++ lib/App.store.ts
@@ -1,12 +1,22 @@
 import type { Action, Reducer, ThunkAction } from "@reduxjs/toolkit";
 import { combineSlices, configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // localStorage
 
 import { listsSlice } from "@/features/lists/listSlice";
 import { RecordsSlice } from "@/features/records/recordSlice";
 import { boardSlice } from "@/features/stores";
 
 import { mainSlice } from "./AppMainSlice";
-
 // import { examSlice } from "./features/exam/ExamSlice";
 // export const listenerMiddleware = createListenerMiddleware();
 
@@ -31,7 +41,15 @@ const combineReducers = combineSlices(
   RecordsSlice,
   mainSlice,
 );
-
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+  // يمكنك تحديد أي slices تحفظها
+  // whitelist: ["list", "record"], // فقط list و record
+  // أو تستثني slices معينة
+  // blacklist: ['someSlice']
+};
 const rootReducer: Reducer<ReturnType<typeof combineReducers>, Action> = (
   state,
   action,
@@ -45,6 +63,7 @@ const rootReducer: Reducer<ReturnType<typeof combineReducers>, Action> = (
 // examSlice
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof combineReducers>;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // `makeStore` encapsulates the store configuration to allow
 // creating unique store instances, which is particularly important for
@@ -52,10 +71,17 @@ export type RootState = ReturnType<typeof combineReducers>;
 // are needed for each request to prevent cross-request state pollution.
 export const makeStore = () => {
   return configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
   });
 };
-
+export const store = makeStore();
+export const persistor = persistStore(store);
 // Infer the return type of `makeStore`
 export type AppStore = ReturnType<typeof makeStore>;
 // Infer the `AppDispatch` type from the store itself
